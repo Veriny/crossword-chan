ddeac54879df9594fc4cdf628abb2793418153d6
Justin Im <20imj68@myahs.org>
2019-12-31 16:39:42 -0800

    Auto stash before merge of "master" and "origin/master"

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import discord
 from discord.ext import commands
 import os
 
-desktop = os.path.join(os.environ['USERPROFILE'], "Desktop")
-token_file = os.path.join(desktop, "token.txt")
-file = open(token_file, 'r')
-BOT_TOKEN = file.readline()
-file.close()
+# desktop = os.path.join(os.environ['USERPROFILE'], "Desktop")
+# token_file = os.path.join(desktop, "token.txt")
+# file = open(token_file, 'r')
+# BOT_TOKEN = file.readline()
+# file.close()
 # print(BOT_TOKEN)
 bot = commands.Bot(command_prefix = "+")
 
@@ -36,4 +36,4 @@ async def reload(ctx, cogname):
 for ext in extensions:
     bot.load_extension(ext)
 
-bot.run(BOT_TOKEN)
+bot.run("NDU4MzQ5NjMyNTIzNTk5ODcy.Xgve0w.aFNtYLsN0aAcHqih5c63uGdf9s4")
