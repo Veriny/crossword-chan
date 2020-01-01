0005d60780589bad464059d7c164cd4a34b35e4d
Justin Im <20imj68@myahs.org>
2019-12-31 19:28:55 -0800

    added ability to search for specific tags

--- htest.py
+++ htest.py
@@ -6,13 +6,13 @@ from discord.ext.commands import bot
 from discord.ext import commands
 
 API_TOKEN = "&api_key=6edceb08a4ab5c6c6d4fb8d65f7db50dec2ffcf6acbb4905d40751bed9cb0660&user_id=500094"
-tags = "&tags=ahegao+azur_lane"
+tags = ""
 
 class Htest(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     async def getH(self, ctx):
-        url = "https://gelbooru.com/index.php?page=dapi&s=post&q=index" + tags + API_TOKEN + "&json=1"
+        url = "https://safebooru.org/index.php?page=dapi&s=post&q=index&json=1"
         r = requests.get(url=url)
         with open('r.json', 'w') as json_file:
             json.dump(r.json(), json_file)
@@ -28,17 +28,23 @@ class Htest(commands.Cog):
 
     @commands.command()
     async def h(self, ctx):
-        url = "https://gelbooru.com/index.php?page=dapi&s=post&q=index" + tags + API_TOKEN + "&json=1"
+        url = "https://safebooru.org/index.php?page=dapi&s=post&q=index"  + API_TOKEN + "&json=1"
         r = requests.get(url=url)
         with open('r.json', 'w') as json_file:
             json.dump(r.json(), json_file)
         data = r.json()
         post = data[random.randint(1,99)]
-        await ctx.channel.send(post['file_url'])
+        await ctx.channel.send("https://safebooru.org//images/2859/" + post['image'])
 
-
-
-
+    @commands.command()
+    async def img(self, ctx, tag):
+        url = "https://safebooru.org/index.php?page=dapi&s=post&q=index" + "&tags={}".format(tag) + API_TOKEN + "&json=1"
+        r = requests.get(url=url)
+        with open('r.json', 'w') as json_file:
+            json.dump(r.json(), json_file)
+        data = r.json()
+        post = data[random.randint(1,99)]
+        await ctx.channel.send("https://safebooru.org//images/{}/".format(post['directory']) + post['image'])
 
 def setup(Bot):
     Bot.add_cog(Htest(Bot))
